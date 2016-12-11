2dfa0d71ea09397ccd2d292b339a5ab262ed4f8f
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2016-12-11 22:36:11 +0100

    test

--- Test/app.js
+++ Test/app.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d'),
     FONT_HEIGHT = 15,
@@ -31,14 +30,6 @@ function drawNumerals() {
          canvas.width/2  + Math.cos(angle)*(HAND_RADIUS) - numeralWidth/2,
          canvas.height/2 + Math.sin(angle)*(HAND_RADIUS) + FONT_HEIGHT/3);
    });
-=======
-const ready = function() {
-    var c = document.getElementById("exemple");
-    (function() {
-        var E;
-=======
->>>>>>> test
-
 var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d'),
     FONT_HEIGHT = 15,
@@ -100,55 +91,13 @@ function drawHands() {
 
 function drawClock() {
    context.clearRect(0,0,canvas.width,canvas.height);
-
-<<<<<<< HEAD
->>>>>>> test
-}
-
-function drawCenter() {
-   context.beginPath();
-   context.arc(canvas.width/2, canvas.height/2, 5, 0, Math.PI*2, true);
-   context.fill();
-}
-
-function drawHand(loc, isHour) {
-   var angle = (Math.PI*2) * (loc/60) - Math.PI/2,
-       handRadius = isHour ? RADIUS - HAND_TRUNCATION-HOUR_HAND_TRUNCATION
-                           : RADIUS - HAND_TRUNCATION;
-
-   context.moveTo(canvas.width/2, canvas.height/2);
-   context.lineTo(canvas.width/2  + Math.cos(angle)*handRadius,
-                  canvas.height/2 + Math.sin(angle)*handRadius);
-   context.stroke();
-}
-
-function drawHands() {
-   var date = new Date,
-       hour = date.getHours();
-   hour = hour > 12 ? hour - 12 : hour;
-   drawHand(hour*5 + (date.getMinutes()/60)*5, true, 0.5);
-   drawHand(date.getMinutes(), false, 0.5);
-   drawHand(date.getSeconds(), false, 0.2);
-}
-
-function drawClock() {
-   context.clearRect(0,0,canvas.width,canvas.height);
-
+
    drawCircle();
    drawCenter();
    drawHands();
    drawNumerals();
 }
 
-=======
-   drawCircle();
-   drawCenter();
-   drawHands();
-   drawNumerals();
-}
-
->>>>>>> test
-// Initialization................................................
 
 context.font = FONT_HEIGHT + 'px Arial';
 loop = setInterval(drawClock, 1000);
