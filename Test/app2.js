5eb6a0fe1cac6bbe36b767465ffb738ed7bff612
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2016-12-11 22:42:18 +0100

    test

--- Test/app2.js
+++ Test/app2.js
@@ -1,154 +1,87 @@
-/*
- * Copyright (C) 2012 David Geary. This code is from the book
- * Core HTML5 Canvas, published by Prentice-Hall in 2012.
- *
- * License:
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation files
- * (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * The Software may not be used to create training material of any sort,
- * including courses, books, instructional videos, presentations, etc.
- * without the express written consent of David Geary.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
-*/
 
-var canvas = document.getElementById('canvas'),
-    context = canvas.getContext('2d'),
-    rubberbandDiv = document.getElementById('rubberbandDiv'),
-    resetButton = document.getElementById('resetButton'),
-    image = new Image(),
-    mousedown = {},
-    rubberbandRectangle = {},
-    dragging = false;
+var context = document.getElementById('canvas').getContext('2d'),
+    startButton = document.getElementById('startButton'),
+    glasspane = document.getElementById('glasspane'),
+    paused = true,
+    circles = [];
 
-// Functions.....................................................
+drawGrid(context, 'lightgray', 10, 10);
 
-function rubberbandStart(x, y) {
-	mousedown.x = x;
-	mousedown.y = y;
+context.lineWidth = 0.5;
+context.font = '32pt Ariel';
 
-	rubberbandRectangle.left = mousedown.x;
-	rubberbandRectangle.top = mousedown.y;
-
-   moveRubberbandDiv();
-   showRubberbandDiv();
-
-	dragging = true;
+for (var i=0; i < 100; ++i) {
+   circles[i] = {
+       x: 100,
+       y: 100,
+       velocityX: 3*Math.random(),
+       velocityY: 3*Math.random(),
+       radius: 50*Math.random(),
+       color: 'rgba(' + (Math.random()*255).toFixed(0) + ', ' +
+                        (Math.random()*255).toFixed(0) + ', ' +
+                        (Math.random()*255).toFixed(0) + ', 1.0)' };
 }
 
-function rubberbandStretch(x, y) {
-   rubberbandRectangle.left = x < mousedown.x ? x : mousedown.x;
-   rubberbandRectangle.top  = y < mousedown.y ? y : mousedown.y;
-
-   rubberbandRectangle.width  = Math.abs(x - mousedown.x),
-   rubberbandRectangle.height = Math.abs(y - mousedown.y);
-
-   moveRubberbandDiv();
-   resizeRubberbandDiv();
+startButton.onclick = function(e) {
+   e.preventDefault();
+   e.stopPropagation();
+   paused = ! paused;
+   startButton.innerText = paused ? 'Start' : 'Stop';
 };
 
-function rubberbandEnd() {
-   var bbox = canvas.getBoundingClientRect();
+glasspane.onmousedown = function(e) {
+   e.preventDefault();
+   e.stopPropagation();
+}
 
-   try {
-      context.drawImage(canvas,
-                        rubberbandRectangle.left - bbox.left,
-                        rubberbandRectangle.top - bbox.top,
-                        rubberbandRectangle.width,
-                        rubberbandRectangle.height,
-                        0, 0, canvas.width, canvas.height);
+context.canvas.onmousedown = function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+};
+
+setInterval(function() {
+   if (!paused) {
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+      drawGrid(context, 'lightgray', 10, 10);
+
+      circles.forEach(function(circle) {
+         context.beginPath();
+         context.arc(circle.x, circle.y, circle.radius, 0, Math.PI*2, false);
+         context.fillStyle = circle.color;
+         context.fill();
+         adjustPosition(circle);
+      });
    }
-   catch (e) {
-      // suppress error message when mouse is released
-      // outside the canvas
+}, 1000 / 60);
+
+function adjustPosition(circle) {
+   if (circle.x + circle.velocityX + circle.radius > context.canvas.width ||
+       circle.x + circle.velocityX - circle.radius < 0)
+      circle.velocityX = -circle.velocityX;
+
+   if (circle.y + circle.velocityY + circle.radius > context.canvas.height ||
+       circle.y + circle.velocityY - circle.radius  < 0)
+      circle.velocityY= -circle.velocityY;
+
+   circle.x += circle.velocityX;
+   circle.y += circle.velocityY;
+}
+
+function drawGrid(context, color, stepx, stepy) {
+   context.strokeStyle = color;
+   context.lineWidth = 0.5;
+
+   for (var i = stepx + 0.5; i < context.canvas.width; i += stepx) {
+      context.beginPath();
+      context.moveTo(i, 0);
+      context.lineTo(i, context.canvas.height);
+      context.stroke();
    }
 
-   resetRubberbandRectangle();
-
-   rubberbandDiv.style.width = 0;
-   rubberbandDiv.style.height = 0;
-
-   hideRubberbandDiv();
-
-   dragging = false;
+   for (var i = stepy + 0.5; i < context.canvas.height; i += stepy) {
+      context.beginPath();
+      context.moveTo(0, i);
+      context.lineTo(context.canvas.width, i);
+      context.stroke();
+   }
 }
-
-function moveRubberbandDiv() {
-   rubberbandDiv.style.top  = rubberbandRectangle.top  + 'px';
-   rubberbandDiv.style.left = rubberbandRectangle.left + 'px';
-}
-
-function resizeRubberbandDiv() {
-   rubberbandDiv.style.width  = rubberbandRectangle.width  + 'px';
-   rubberbandDiv.style.height = rubberbandRectangle.height + 'px';
-}
-
-function showRubberbandDiv() {
-   rubberbandDiv.style.display = 'inline';
-}
-
-function hideRubberbandDiv() {
-   rubberbandDiv.style.display = 'none';
-}
-
-function resetRubberbandRectangle() {
-   rubberbandRectangle = { top: 0, left: 0, width: 0, height: 0 };
-}
-
-// Event handlers...............................................
-
-canvas.onmousedown = function (e) {
-   var x = e.x || e.clientX,
-       y = e.y || e.clientY;
-
-	e.preventDefault();
-   rubberbandStart(x, y);
-};
-
-window.onmousemove = function (e) {
-   var x = e.x || e.clientX,
-       y = e.y || e.clientY;
-
-	e.preventDefault();
-	if (dragging) {
-      rubberbandStretch(x, y);
-    }
-}
-
-window.onmouseup = function (e) {
-	e.preventDefault();
-   rubberbandEnd();
-}
-
-// Event handlers..............................................
-
-image.onload = function () {
-	context.drawImage(image, 0, 0, canvas.width, canvas.height);
-};
-
-resetButton.onclick = function(e) {
-   context.clearRect(0, 0, context.canvas.width,
-                            context.canvas.height);
-   context.drawImage(image, 0, 0, canvas.width, canvas.height);
-};
-
-// Initialization..............................................
-
-image.src = 'lel.png';
