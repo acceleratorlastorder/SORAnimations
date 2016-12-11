fa24634d73a7664b238847a71114e0bc275c0470
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2016-12-11 23:33:38 +0100

    test

--- Test/app2.js
+++ Test/app2.js
@@ -1,87 +1,40 @@
 
-var context = document.getElementById('canvas').getContext('2d'),
-    startButton = document.getElementById('startButton'),
-    glasspane = document.getElementById('glasspane'),
-    paused = true,
-    circles = [];
+  //Global variables
+  var myImage = new Image(); // Create a new blank image.
 
-drawGrid(context, 'lightgray', 10, 10);
+  // Load the image and display it.
+  function displayImage() {
 
-context.lineWidth = 0.5;
-context.font = '32pt Ariel';
+    // Get the canvas element.
+    canvas = document.getElementById("myCanvas");
 
-for (var i=0; i < 100; ++i) {
-   circles[i] = {
-       x: 100,
-       y: 100,
-       velocityX: 3*Math.random(),
-       velocityY: 3*Math.random(),
-       radius: 50*Math.random(),
-       color: 'rgba(' + (Math.random()*255).toFixed(0) + ', ' +
-                        (Math.random()*255).toFixed(0) + ', ' +
-                        (Math.random()*255).toFixed(0) + ', 1.0)' };
-}
+    // Make sure you got it.
+    if (canvas.getContext) {
 
-startButton.onclick = function(e) {
-   e.preventDefault();
-   e.stopPropagation();
-   paused = ! paused;
-   startButton.innerText = paused ? 'Start' : 'Stop';
-};
+      // Specify 2d canvas type.
+      ctx = canvas.getContext("2d");
 
-glasspane.onmousedown = function(e) {
-   e.preventDefault();
-   e.stopPropagation();
-}
+      // When the image is loaded, draw it.
+      myImage.onload = function() {
 
-context.canvas.onmousedown = function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-};
+        // Load the image into the context.
+        ctx.drawImage(myImage, 0, 0);
 
-setInterval(function() {
-   if (!paused) {
-      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-      drawGrid(context, 'lightgray', 10, 10);
+        // Get and modify the image data.
+        changeImage();
+      }
 
-      circles.forEach(function(circle) {
-         context.beginPath();
-         context.arc(circle.x, circle.y, circle.radius, 0, Math.PI*2, false);
-         context.fillStyle = circle.color;
-         context.fill();
-         adjustPosition(circle);
-      });
-   }
-}, 1000 / 60);
+      // Define the source of the image.
+      myImage.src = "http://samples.msdn.microsoft.com/workshop/samples/canvas/kestral.png";
+    }
+  }
 
-function adjustPosition(circle) {
-   if (circle.x + circle.velocityX + circle.radius > context.canvas.width ||
-       circle.x + circle.velocityX - circle.radius < 0)
-      circle.velocityX = -circle.velocityX;
+  function changeImage() {
 
-   if (circle.y + circle.velocityY + circle.radius > context.canvas.height ||
-       circle.y + circle.velocityY - circle.radius  < 0)
-      circle.velocityY= -circle.velocityY;
-
-   circle.x += circle.velocityX;
-   circle.y += circle.velocityY;
-}
-
-function drawGrid(context, color, stepx, stepy) {
-   context.strokeStyle = color;
-   context.lineWidth = 0.5;
-
-   for (var i = stepx + 0.5; i < context.canvas.width; i += stepx) {
-      context.beginPath();
-      context.moveTo(i, 0);
-      context.lineTo(i, context.canvas.height);
-      context.stroke();
-   }
-
-   for (var i = stepy + 0.5; i < context.canvas.height; i += stepy) {
-      context.beginPath();
-      context.moveTo(0, i);
-      context.lineTo(context.canvas.width, i);
-      context.stroke();
-   }
-}
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = "100";
+    ctx.beginPath();
+    ctx.arc(100, 100, 150, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.stroke();
+  }
