d331b61d79ebca9e80336bb366c89bceca4b0455
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2016-12-12 17:21:46 +0100

    update

--- Test/usefullfunction.js
+++ Test/usefullfunction.js
@@ -1,12 +1,25 @@
 function adjustPosition(circle) {
-   if (circle.x + circle.velocityX + circle.radius > context.canvas.width ||
-       circle.x + circle.velocityX - circle.radius < 0)
-      circle.velocityX = -circle.velocityX;
+    if (circle.x + circle.velocityX + circle.radius > context.canvas.width ||
+        circle.x + circle.velocityX - circle.radius < 0)
+        circle.velocityX = -circle.velocityX;
 
-   if (circle.y + circle.velocityY + circle.radius > context.canvas.height ||
-       circle.y + circle.velocityY - circle.radius  < 0)
-      circle.velocityY= -circle.velocityY;
+    if (circle.y + circle.velocityY + circle.radius > context.canvas.height ||
+        circle.y + circle.velocityY - circle.radius < 0)
+        circle.velocityY = -circle.velocityY;
 
-   circle.x += circle.velocityX;
-   circle.y += circle.velocityY;
+    circle.x += circle.velocityX;
+    circle.y += circle.velocityY;
+}
+
+for (var i = 0; i < 100; ++i) {
+    circles[i] = {
+        x: 100,
+        y: 100,
+        velocityX: 3 * Math.random(),
+        velocityY: 3 * Math.random(),
+        radius: 50 * Math.random(),
+        color: 'rgba(' + (Math.random() * 255).toFixed(0) + ', ' +
+            (Math.random() * 255).toFixed(0) + ', ' +
+            (Math.random() * 255).toFixed(0) + ', 1.0)'
+    };
 }
