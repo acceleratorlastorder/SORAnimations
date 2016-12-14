0bd2566a4aeeb886e72ae2d7610d106628638ed3
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2016-12-14 15:04:42 +0100

    update

--- Test/app.js
+++ Test/app.js
@@ -1,72 +1,104 @@
-var canvas = document.getElementById('canvas'),
+var canvas = document.getElementById('timercanvas'),
     context = canvas.getContext('2d'),
-    FONT_HEIGHT = 15,
+    FONT_HEIGHT = 10,
     MARGIN = 35,
-    HAND_TRUNCATION = canvas.width/25,
-    HOUR_HAND_TRUNCATION = canvas.width/10,
+    HAND_TRUNCATION = canvas.width / 25,
+    HOUR_HAND_TRUNCATION = canvas.width / 10,
     NUMERAL_SPACING = 20,
-    RADIUS = canvas.width/2 - MARGIN,
+    RADIUS = canvas.width / 2 - MARGIN,
     HAND_RADIUS = RADIUS + NUMERAL_SPACING;
 
 // Functions.....................................................
 
 function drawCircle() {
-   context.beginPath();
-   context.arc(canvas.width/2, canvas.height/2,
-               RADIUS, 0, Math.PI*2, true);
-   context.stroke();
+    context.beginPath();
+    context.arc(canvas.width / 2, canvas.height / 2,
+        RADIUS, 0, Math.PI * 2, true);
+    context.stroke();
 }
 
 function drawNumerals() {
-   var numerals = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
-       angle = 0,
-       numeralWidth = 0;
+    var numerals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+        angle = 0,
+        numeralWidth = 0;
 
-   numerals.forEach(function(numeral) {
-      angle = Math.PI/6 * (numeral-3);
-      numeralWidth = context.measureText(numeral).width;
-      context.fillText(numeral,
-         canvas.width/2  + Math.cos(angle)*(HAND_RADIUS) - numeralWidth/2,
-         canvas.height/2 + Math.sin(angle)*(HAND_RADIUS) + FONT_HEIGHT/3);
-   });
+    numerals.forEach(function(numeral) {
+        angle = Math.PI / 15 * (numeral - 7.5);
+        numeralWidth = context.measureText(numeral).width;
+        context.fillText(numeral,
+            canvas.width / 2 + Math.cos(angle) * (HAND_RADIUS) - numeralWidth / 2,
+            canvas.height / 2 + Math.sin(angle) * (HAND_RADIUS) + FONT_HEIGHT / 3);
+    });
 }
 
 function drawCenter() {
-   context.beginPath();
-   context.arc(canvas.width/2, canvas.height/2, 5, 0, Math.PI*2, true);
-   context.fill();
+    context.beginPath();
+    context.arc(canvas.width / 2, canvas.height / 2, 5, 0, Math.PI * 2, true);
+    context.fill();
 }
 
 function drawHand(loc, isHour) {
-   var angle = (Math.PI*2) * (loc/60) - Math.PI/2,
-       handRadius = isHour ? RADIUS - HAND_TRUNCATION-HOUR_HAND_TRUNCATION
-                           : RADIUS - HAND_TRUNCATION;
+    var angle = (Math.PI * 2) * (loc / 60) - Math.PI / 2,
+        handRadius = isHour ? RADIUS - HAND_TRUNCATION - HOUR_HAND_TRUNCATION :
+        RADIUS - HAND_TRUNCATION;
 
-   context.moveTo(canvas.width/2, canvas.height/2);
-   context.lineTo(canvas.width/2  + Math.cos(angle)*handRadius,
-                  canvas.height/2 + Math.sin(angle)*handRadius);
-   context.stroke();
+    context.moveTo(canvas.width / 2, canvas.height / 2);
+    context.lineTo(canvas.width / 2 + Math.cos(angle) * handRadius,
+        canvas.height / 2 + Math.sin(angle) * handRadius);
+    context.stroke();
 }
 
 function drawHands() {
-   var date = new Date,
-       hour = date.getHours();
-   hour = hour > 12 ? hour - 12 : hour;
-   drawHand(hour*5 + (date.getMinutes()/60)*5, true, 0.5);
-   drawHand(date.getMinutes(), false, 0.5);
-   drawHand(date.getSeconds(), false, 0.2);
+    var date = new Date,
+        sec = date.getHours();
+    sec = sec > 30 ? sec - 30 : sec;
+    drawHand(sec * 7 + (date.getSeconds() * 2), false, 4);
 }
 
 function drawClock() {
-   context.clearRect(0,0,canvas.width,canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-   drawCircle();
-   drawCenter();
-   drawHands();
-   drawNumerals();
+
+    drawCircle();
+    drawCenter();
+    drawHands();
+    drawNumerals();
 }
 
 // Initialization................................................
 
+/*
+i=0;
+result = "fini"
+function timer(){
+i++
+if (i==30) {
+    console.log(i);
+  return result;
+}
+else {
+      console.log(i);
+}
+}
+*/
 context.font = FONT_HEIGHT + 'px Arial';
-loop = setInterval(drawClock, 1000);
+
+function start() {
+    result = "fini"
+    for (let i = 0; i < 30; i++) {
+        if (i == 30) {
+            console.log(i);
+            return result;
+        } else {
+            loop = setInterval(drawClock, 1000);
+            console.log(i);
+        }
+    }
+}
+/*
+
+function start()
+{
+   alert("let's start");
+   return true;
+}*/
