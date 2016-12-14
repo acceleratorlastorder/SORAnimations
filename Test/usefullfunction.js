2f81e438d5c8650752e8e9a875c0872339c4dd52
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2016-12-14 09:05:55 +0100

    update usefullfunction

--- Test/usefullfunction.js
+++ Test/usefullfunction.js
@@ -23,3 +23,43 @@ for (var i = 0; i < 100; ++i) {
             (Math.random() * 255).toFixed(0) + ', 1.0)'
     };
 }
+/*
+
+this.context = canvas.getContext('2d');
+this.gameName = gameName;
+this.sprites = [];
+this.keyListeners = [];
+
+// High scores
+
+this.HIGH_SCORES_SUFFIX = '_highscores';
+*/
+// High scores................................................................
+
+  // Returns an array of high scores from local storage.
+
+  getHighScores: function () {
+     var key = this.gameName + this.HIGH_SCORES_SUFFIX,
+         highScoresString = localStorage[key];
+
+     if (highScoresString == undefined) {
+        localStorage[key] = JSON.stringify([]);
+     }
+     return JSON.parse(localStorage[key]);
+  },
+
+  // Sets the high score in local storage.
+
+  setHighScore: function (highScore) {
+     var key = this.gameName + this.HIGH_SCORES_SUFFIX,
+         highScoresString = localStorage[key];
+
+     highScores.unshift(highScore);
+     localStorage[key] = JSON.stringify(highScores);
+  },
+
+  // Removes the high scores from local storage.
+
+  clearHighScores: function () {
+     localStorage[this.gameName + this.HIGH_SCORES_SUFFIX] = JSON.stringify([]);
+  },
