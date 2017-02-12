376d22a2287dc0c3e561081d496d3650f2b441a7
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-02-12 17:58:57 +0100

    update

--- baywatch/js/application-jquery.js
+++ baywatch/js/application-jquery.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-let score = 0;
+    let score = 0;
     $('#startButton').click(function() {
         mobgenerator();
-            $("#glasspane").remove();
-
+        $("#glasspane").fadeTo("slow", 0, function() {
+            $(this).remove();
+        });
 
         $('#canvas div:nth-child(2)').on('click', 'span', function() {
             $(this).remove();
@@ -27,28 +28,24 @@ mobgenerator = function() {
 }
 
 randomrespawn = function(x) {
-  let random = Math.random();
+    let random = Math.random();
     if (x == 1) {
-      if (random < 0.25) {
-        $('#canvas div:nth-child(2)').append('<span></span>');
-        console.log("blond created");
-      }
-      else {
-        $('#canvas div:first').append('<span></span>');
-        console.log("shark created");
-      }
-    }
-   else if (x == 0) {
-     if (random < 0.25) {
-       $('#canvas div:first').append('<span></span>');
-       console.log("shark created");
-     }
-     else {
-       $('#canvas div:nth-child(2)').append('<span></span>');
-       console.log("blond created");
-     }
-   }
-    else {
+        if (random < 0.25) {
+            $('#canvas div:nth-child(2)').append('<span></span>');
+            console.log("blond created");
+        } else {
+            $('#canvas div:first').append('<span></span>');
+            console.log("shark created");
+        }
+    } else if (x == 0) {
+        if (random < 0.25) {
+            $('#canvas div:first').append('<span></span>');
+            console.log("shark created");
+        } else {
+            $('#canvas div:nth-child(2)').append('<span></span>');
+            console.log("blond created");
+        }
+    } else {
         console.log("unexpected value, please just play the game instead of trying weird things on it :)");
     }
 
