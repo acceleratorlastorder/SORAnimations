dbd8ebfa16995b142730a82627e9b9e369839ef3
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-02-12 16:27:56 +0100

    charset added in the index.html to remove the console log alert

--- baywatch/js/application-jquery.js
+++ baywatch/js/application-jquery.js
@@ -1,16 +1,37 @@
-
-
 $(document).ready(function() {
 
-  $('#startButton').click(function() {
+    $('#startButton').click(function() {
+        mobgenerator();
+
+        $('#canvas div:nth-child(2)').on('click', 'span', function() {
+            $(this).remove();
+            randomrespawn(0);
+        });
+        $('#canvas div:first').on('click', 'span', function() {
+            $(this).remove();
+            randomrespawn(1);
+        });
+
+    });
+});
+
+mobgenerator = function() {
+    for (var i = 0; i < 10; i++) {
+        $('#canvas div:nth-child(2)').append('span');
+        $('#canvas div:first').append('span');
+
+    }
     $('#canvas div:nth-child(2)').addClass('goodLeaves');
     $('#canvas div:first').addClass('fallingLeaves');
-
-    $('#canvas div').click(function() {
-        $(this).hide();
-    });
+}
 
 
-  });
 
-});
+randomrespawn = function(x) {
+    if (x == 1) {
+        console.log("salut");
+    } else {
+        console.log("lel");
+    }
+
+}
